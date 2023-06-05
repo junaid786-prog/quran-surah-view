aef8471be6a571a8eee36c0e64064e337c3aafdf
junaid786-prog <junaidshoaib555@gmail.com>
2023-06-05 20:28:46 +0500

    initialized

---
+++ app/src/main/java/com/example/holyquran/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.holyquran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.navigationdrawer.*;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
